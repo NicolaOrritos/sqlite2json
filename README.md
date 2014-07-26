fc2d5348d34711568e8fe542767bb6783e492d78
Nicola Orritos <nicola.orritos@gmail.com>
2014-07-26 17:07:00 +0200

    Bootstrapped a simple project

---
+++ README.md
@@ -0,0 +1,44 @@
+# sqlite2json [![Build Status](https://secure.travis-ci.org/NicolaOrritos/sqlite2json.png?branch=master)](http://travis-ci.org/NicolaOrritos/sqlite2json)
+
+> The best module ever.
+
+
+## Getting Started
+
+Install the module with: `npm install sqlite2json`
+
+```js
+var sqlite2json = require('sqlite2json');
+sqlite2json.awesome(); // "awesome"
+```
+
+Install with cli command
+
+```sh
+$ npm install -g sqlite2json
+$ sqlite2json --help
+$ sqlite2json --version
+```
+
+
+
+
+## Documentation
+
+_(Coming soon)_
+
+
+## Examples
+
+_(Coming soon)_
+
+
+## Contributing
+
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com).
+
+
+## License
+
+Copyright (c) 2014 Nicola Orritos  
+Licensed under the MIT license.
---
+++ test/sqlite2json_test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var sqlite2json = require('../lib/sqlite2json.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports.sqlite2json = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no args': function(test) {
+    test.expect(1);
+    // tests here
+    test.equal(sqlite2json.awesome(), 'awesome', 'should be awesome.');
+    test.done();
+  }
+};
