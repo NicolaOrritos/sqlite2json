686441130cf4cb9112578ce63e0d75fd56dd98dc
Nicola Orritos <nicola.orritos@applixgroup.com>
2016-03-07 11:38:11 +0100

    Added some more tests

--- test/sqlite2json_test.js
+++ test/sqlite2json_test.js
@@ -29,62 +29,63 @@ exports.sqlite2json =
         // setup here
         done();
     },
-    
+
     'simple': function(test)
     {
         // Test data created as follows:
         /* CREATE TABLE foo(x INTEGER PRIMARY KEY ASC, y TEXT);
            INSERT INTO foo(x, y) VALUES(12, "hello"); */
-        
-        
-        test.expect(5);
-        
-        
+
+
         var s2j = new S2J('test/test.sqlite');
-        
+
         s2j.addQuery('query1', 'SELECT * FROM foo');
-        
-        
+
+
         test.ok(s2j);
-        
-        
+
+
 
         s2j.run(function(err, json)
         {
             test.ifError(err);
             test.ok(json);
-            
+
             var data = JSON.parse(json);
-            
+
             test.ok(data);
             test.ok(data.query1);
-            
+
+            test.ok(data.query1[0]);
+            test.ok(data.query1[0].x === 12);
+            test.ok(data.query1[0].y === 'hello');
+
             test.done();
         });
     },
-    
+
     'no queries': function(test)
     {
         test.expect(5);
-        
-        
+
+
         var s2j = new S2J('test/test.sqlite');
-        
-        
+
+
         test.ok(s2j);
-        
-        
+
+
 
         s2j.run(function(err, json)
         {
             test.ifError(err);
             test.ok(json);
-            
+
             var data = JSON.parse(json);
-            
+
             test.ok(data);
             test.deepEqual(data, {});
-            
+
             test.done();
         });
     }
